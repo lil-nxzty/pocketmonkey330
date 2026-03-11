7d6a4b7ffe4a64581b334a6b03f1ab0c87ebba16
ojse <111207279+lil-nxzty@users.noreply.github.com>
2026-03-11 02:40:50 +0000

    sorry for late

---
+++ script.js
@@ -0,0 +1,26 @@
+// ── PROVIDED STARTER CODE — do not modify ──────────────────
+async function fetchData() {
+  try {
+    const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+    const response   = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+
+    const data          = await response.json();
+    const pokemonSprite = data.sprites.front_default;
+    const imgElement    = document.getElementById("pokemonSprite");
+
+    imgElement.src           = pokemonSprite;
+    imgElement.style.display = "block";
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function handleSearch() {
+    let poki = getElementById("pokemonName").value;
+    fetchData(poki);
+    
+}
